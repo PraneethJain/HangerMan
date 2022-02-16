72537e5c855ec0601ece5c6dbed9c4aa32f2e58c
PraneethJain <praneethjain005@gmail.com>
2022-02-16 22:47:37 +0530

    First version finished

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame as pg
 import os
 from rich import print
+from time import sleep
 
 
 pg.init()
 pg.display.set_caption("Hanger Man")
 screen=pg.display.set_mode((800,600))
 clock = pg.time.Clock()
+bg=pg.image.load(os.path.join("Assets","bg.jpg")).convert_alpha()
+scale=2
+bg=pg.transform.scale(bg,(bg.get_width()/scale,bg.get_height()/scale))
+bg_pos = screen.get_width()/2-bg.get_width()/2, screen.get_height()/2-bg.get_height()/2+150
 
 letters = [chr(i) for i in range(65,91)]
 text = ''
@@ -16,16 +21,18 @@ largefont = pg.font.Font(os.path.join("Fonts","dpcomic.ttf"),70)
 start_surf = largefont.render("Enter word and press enter",False,'Blue')
 center = screen.get_width()/2,screen.get_height()/2
 
+attempts_left=7
 status=0
 while True:
     
     if pg.event.get(pg.QUIT): raise SystemExit
-    screen.fill((0,0,0))
+    screen.fill("Black")
+    screen.blit(bg,bg_pos)
     
     events = pg.event.get()
     
     if status==0:
-        screen.blit(start_surf,(center[0]-start_surf.get_width()/2,center[1]-start_surf.get_height()/2-50))
+        screen.blit(start_surf,(center[0]-start_surf.get_width()/2,center[1]-start_surf.get_height()/2-200))
         for event in events:
             if event.type==pg.KEYDOWN:
                 if event.key == pg.K_BACKSPACE:
@@ -39,7 +46,7 @@ while True:
                     status=1
 
         word_surf = largefont.render(word.capitalize(),False,"Orange")
-        screen.blit(word_surf,(center[0]-word_surf.get_width()/2,center[1]-word_surf.get_height()/2+40))
+        screen.blit(word_surf,(center[0]-word_surf.get_width()/2,center[1]-word_surf.get_height()/2-100))
 
     
     if status==1:
@@ -66,11 +73,35 @@ while True:
                         j = replaced.find(event.unicode.upper())
                         replaced=replaced.replace(word[j],'0',1)
                         maintext=maintext[:2*j]+event.unicode.upper()+maintext[2*j+1:]
+                else:
+                    attempts_left -= 1
 
         main = largefont.render(maintext,False,"Pink")
         text_surf = myfont.render(text,False,(0,128,128))
+        attempts=myfont.render(f'Attemps Left: {attempts_left}',False,"Purple")
         screen.blit(main,(center[0]-main.get_width()/2,center[1]-main.get_height()/2))
         screen.blit(text_surf,(0,0))
+        screen.blit(attempts,(screen.get_width()-attempts.get_width(),attempts.get_height()/2))
+        
+        if attempts_left == 0:
+            game_over = largefont.render("The word was: ",False,"Red")
+            pg.display.flip()
+            sleep(1)
+            status=2
+            
+        if set(guessed)==set(word):
+            you_win = largefont.render("You Win!",False,"White")
+            pg.display.flip()
+            sleep(1)
+            status=3
+            
+    if status==2:
+        screen.blit(word_surf,(center[0]-word_surf.get_width()/2,center[1]-word_surf.get_height()/2))
+        screen.blit(game_over,(center[0]-game_over.get_width()/2,center[1]-game_over.get_height()/2-80))
+    
+    if status==3:
+        screen.blit(you_win,(center[0]-you_win.get_width()/2,center[1]-you_win.get_height()/2))
+    
     
     clock.tick(60)
     pg.display.flip()
